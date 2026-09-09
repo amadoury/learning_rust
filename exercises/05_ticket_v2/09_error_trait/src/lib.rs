c9dbe67fede557ac9f737c8db7f26bec9944e1f0
amadou <amadou.diallo@inrap.fr>
2026-09-09 16:41:42 +0200

    few exercise 05 are done

--- exercises/05_ticket_v2/09_error_trait/src/lib.rs
+++ exercises/05_ticket_v2/09_error_trait/src/lib.rs
@@ -3,17 +3,42 @@
 //  The docs for the `std::fmt` module are a good place to start and look for examples:
 //  https://doc.rust-lang.org/std/fmt/index.html#write
 
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::fmt::Error;
+
+#[derive(Debug)]
 enum TicketNewError {
     TitleError(String),
     DescriptionError(String),
 }
+
+impl Display for TicketNewError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        match self {
+            TicketNewError::DescriptionError(s) => write!(f, "{}", s),
+            TicketNewError::TitleError(s) => write!(f, "{}", s)
+        }
+    }
+}
+
+
+impl std::error::Error for TicketNewError {}
+
 
 // TODO: `easy_ticket` should panic when the title is invalid, using the error message
 //   stored inside the relevant variant of the `TicketNewError` enum.
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    match Ticket::new(title.clone(), description, status.clone()) {
+        Ok(ticket) => ticket,
+        Err(err) => 
+        match err {
+            TicketNewError::DescriptionError(s) => Ticket::new(title,  "Description not provided".to_string(), status).unwrap(),
+            TicketNewError::TitleError(s) => panic!("{s}")
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
--- exercises/05_ticket_v2/13_try_from/src/lib.rs
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -1,11 +1,45 @@
 // TODO: Implement `TryFrom<String>` and `TryFrom<&str>` for `Status`.
 //  The parsing should be case-insensitive.
 
+use crate::Status::{Done, InProgress, ToDo};
+
 #[derive(Debug, PartialEq, Clone)]
 enum Status {
     ToDo,
     InProgress,
     Done,
+}
+
+impl TryFrom<String> for Status {
+    type Error = String;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        if value.to_lowercase() == "todo"  {
+            return Ok(ToDo)
+        }
+        if value.to_lowercase() == "inprogress" {
+            return Ok(InProgress)
+        }
+        if value .to_lowercase() == "done" { 
+            return Ok(Done)
+        }
+        Err("Error".to_string())  
+    }
+}
+
+impl TryFrom<&str> for Status {
+    type Error = String;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if value.to_lowercase() == "todo"  {
+            return Ok(ToDo)
+        }
+        if value.to_lowercase() == "inprogress" {
+            return Ok(InProgress)
+        }
+        if value .to_lowercase() == "done" { 
+            return Ok(Done)
+        }
+        Err("Error".to_string())  
+    }
 }
 
 #[cfg(test)]
